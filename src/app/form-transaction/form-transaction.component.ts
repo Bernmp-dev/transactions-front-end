8c66e1b9ef80b4a62ad5ab857e82c80e0d4d6c95
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-30 23:10:32 -0300

    feat: form-transaction

--- src/app/form-transaction/form-transaction.component.ts
+++ src/app/form-transaction/form-transaction.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { __values } from 'tslib';
+import { TransactionsService } from '../services/transactions.service';
+import { TransactionEntity } from '../TransactionEntity';
 
 @Component({
   selector: 'app-form-transaction',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ 
+    ReactiveFormsModule,
+    HttpClientModule,
+    CommonModule
+  ],
+  providers: [ TransactionsService ],
   templateUrl: './form-transaction.component.html',
   styleUrl: './form-transaction.component.scss'
 })
 export class FormTransactionComponent {
+  constructor(private transactionService: TransactionsService){
+
+  }
+
+  transactionsArray: TransactionEntity[] = [];
+  
+  @Input()
   transactionForm = new FormGroup({
     category: new FormControl('', Validators.required),
     value: new FormControl(0, [Validators.required, Validators.min(0.01)]),
@@ -23,7 +40,44 @@ export class FormTransactionComponent {
     });
   }
 
-  onSubmit() {
-    console.log(this.transactionForm.value);
+  addTransaction(transaction: FormGroup['value']) {
+    this.transactionsArray.push(transaction);
   }
+
+  insertTransaction(transaction: FormGroup["value"]) {
+
+    this.transactionService
+      .insertTransaction(transaction)
+      .subscribe({
+        
+        next: () => {
+          alert("transaction saved successfully")
+          this.resetInputs()
+        },
+        
+        error: (httpResponse: HttpErrorResponse) => {
+          console.log(httpResponse);
+          alert(httpResponse.error)
+        },
+  })
+  }
+
+  insertTransactionBatch() {
+
+    this.transactionService
+      .insertTransactionBatch(this.transactionsArray)
+      .subscribe({
+        
+        next: () => {
+          alert("transaction saved successfully")
+          this.resetInputs()
+        },
+        
+        error: (httpResponse: HttpErrorResponse) => {
+          console.log(httpResponse);
+          alert(httpResponse.error)
+        },
+  })
+  }
+
 }
